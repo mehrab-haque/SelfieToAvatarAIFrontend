dd538ccf0c4f4589bffdb3bcd390c32f581f2087
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-05-01 17:45:27 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { ToastContainer, toast as oldToast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Selfie from "./components/Selfie";
+
+var showToast, getApiUrl;
+
+
+function App() {
+
+  showToast = (message) => {
+    oldToast.dark(message, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <>
+      <Selfie/>
+      <ToastContainer />
+    </>
+  );
+}
+export default App;
+export { showToast, getApiUrl };
---
+++ src/components/Selfie.js
@@ -0,0 +1,105 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import React from 'react'
+import Webcam from 'react-webcam'
+import { CameraOptions, useFaceDetection } from 'react-use-face-detection';
+import FaceDetection from '@mediapipe/face_detection';
+import { Camera } from '@mediapipe/camera_utils';
+
+const Selfie=props=>{
+
+    var width = window.innerWidth-50;
+    var height = window.innerHeight-200;
+    width=Math.min(width,height)
+    height=width 
+
+
+    const { webcamRef, boundingBox, isLoading, detected, facesDetected } = useFaceDetection({
+        faceDetectionOptions: {
+          model: 'short',
+        },
+        faceDetection: new FaceDetection.FaceDetection({
+          locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`,
+        }),
+        camera: ({ mediaSrc, onFrame }) =>
+          new Camera(mediaSrc, {
+            onFrame,
+            width,
+            height,
+          }),
+      });
+
+    
+    const selfieClick=async ()=>{
+        const imageSrc = webcamRef.current.getScreenshot();
+        console.log(imageSrc)
+    }
+      
+
+    return(
+        <div style={{
+            width:'100vw',
+            height:'100vh',
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center'
+        }}>
+            <div style={{
+                width:`${width+50}px`,
+                display:'block'
+            }}>
+                <div open={true}>
+                    <DialogTitle>
+                        Make My Avatar <span
+                            style={{
+                                display:'inline-block',
+                                height:'15px',
+                                transform:'translateY(2px)',
+                                borderRadius:'50%',
+                                width:'15px',
+                                backgroundColor:isLoading?'#aaaaaa':(facesDetected!==1?'red':'#00cf00')
+                            }}/>
+                    </DialogTitle>
+                    <DialogContent>
+                    <div style={{ width, height, position: 'relative',overflow:'hidden' }}>
+                        {boundingBox.map((box, index) => (
+                        <div
+                            key={`${index + 1}`}
+                            style={{
+                            border: '4px solid red',
+                            position: 'absolute',
+                            top: `${box.yCenter * 100}%`,
+                            left: `${box.xCenter * 100}%`,
+                            width: `${box.width * 100}%`,
+                            height: `${box.height * 100}%`,
+                            zIndex: 1,
+                            }}
+                        />
+                        ))}
+                        <Webcam
+                            ref={webcamRef}
+                            forceScreenshotSourceSize
+                            screenshotFormat="image/png"
+                            style={{
+                                height,
+                                width,
+                                position: 'absolute',
+                            }}
+                        />
+                    </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            color='primary'
+                            variant='outlined'
+                            onClick={selfieClick}
+                            >
+                            Take Selfie
+                        </Button>
+                    </DialogActions>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Selfie
